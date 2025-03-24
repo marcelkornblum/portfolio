1b623659d0d5775a6571c2b84c047d6a96414360
marcelkornblum <me@marcelkornblum.com>
2025-03-24 23:23:03 +0000

    updates

--- nextjs/next.config.js
+++ nextjs/next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    assetPrefix: '/portfolio/',
-    basePath: '/portfolio',
+    // assetPrefix: '/portfolio/',
+    // basePath: '/portfolio',
+    // reactStrictMode: true,
+    // images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
--- nextjs/sanity.config.ts
+++ nextjs/sanity.config.ts
@@ -1,11 +1,11 @@
 import { createClient } from "@sanity/client";
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID; // Replace with your project ID
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET; // Replace with your dataset name
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
 
 export const client = createClient({
-  projectId,
-  dataset,
-  apiVersion: "2023-05-03", // Use a recent API version
-  useCdn: true, // `false` if you want to ensure fresh data
+    projectId,
+    dataset,
+    apiVersion: "2023-05-03", // Use a recent API version
+    useCdn: true, // `false` if you want to ensure fresh data
 });
