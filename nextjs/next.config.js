940e63ec1d476ea2e682ee7faca7fa1cd9f6cdda
marcelkornblum <me@marcelkornblum.com>
2025-03-24 19:01:38 +0000

    try a pages config

---
+++ nextjs/next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    assetPrefix: '/portfolio/',
+    basePath: '/portfolio',
+};
+
+module.exports = nextConfig;
